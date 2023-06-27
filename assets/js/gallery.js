2ddadf438fd16e3c243fdb6d78469af89596126b
David Robb <66834033+drobb2020@users.noreply.github.com>
2023-06-27 08:28:25 -0400

    rearranged files for the gallery

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,33 +1,70 @@
-// Specify the directory path where your photos are located
-const photoDirectory = '../../photogallery/2020-2021/'
-
-// Get a reference to the gallery container element
-const galleryContainer = document.getElementById('gallery')
-
-// Function to fetch the list of photos from the subdirectory
-async function fetchPhotos() {
-  try {
-    const response = await fetch(photoDirectory)
-    const html = await response.text()
-    const parser = new DOMParser()
-    const doc = parser.parseFromString(html, 'text/html')
-    const photoLinks = doc.querySelectorAll('a')
-
-    // Iterate over the photo links and create gallery elements
-    photoLinks.forEach(link => {
-      const photoUrl = link.href
-
-      // Create a new image element for each photo
-      const image = document.createElement('img')
-      image.src = photoUrl
-
-      // Append the image element to the gallery container
-      galleryContainer.appendChild(image)
-    })
-  } catch (error) {
-    console.error('Error fetching photos:', error)
-  }
+// Function to recursively fetch all photo files in subfolders
+function getPhotosFromFolder(folderPath) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest()
+    xhr.open('GET', folderPath, true)
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          const html = new DOMParser().parseFromString(
+            xhr.responseText,
+            'text/html'
+          )
+          const imageElements = html.querySelectorAll(
+            'a[href$=".jpg"], a[href$=".png"], a[href$=".jpeg"]'
+          )
+          const photoUrls = Array.from(imageElements).map(
+            element => element.href
+          )
+          resolve(photoUrls)
+        } else {
+          reject(xhr.status)
+        }
+      }
+    }
+    xhr.send()
+  })
 }
 
-// Call the fetchPhotos function to populate the gallery
-fetchPhotos()
+// Function to display the photos in the gallery
+function displayPhotos(photoUrls) {
+  const photoGallery = document.getElementById('photoGallery')
+  photoUrls.forEach(url => {
+    const photo = document.createElement('img')
+    photo.src = url
+    photo.className = 'photo'
+
+    photo.addEventListener('click', () => {
+      // popup stuff
+      popup.style.transform = 'translateY(0)'
+      selectedImage.src = url
+      selectedImage.alt = '2nd Russell Scouting image'
+    })
+
+    photoGallery.appendChild(photo)
+  })
+}
+
+// Fetch photos from subfolders and display them
+const parentFolder = '../../photogallery/'
+const subfolders = ['2019-2020', '2020-2021', '2021-2022', '2022-2023',]
+
+const fetchPromises = subfolders.map(subfolder => {
+  const folderPath = parentFolder + subfolder + '/'
+  return getPhotosFromFolder(folderPath)
+})
+
+Promise.all(fetchPromises)
+  .then(results => {
+    const photoUrls = results.flat()
+    displayPhotos(photoUrls)
+  })
+  .catch(error => {
+    console.error('Error fetching photos:', error)
+  })
+
+popup.addEventListener('click', () => {
+  popup.style.transform = `translateY(-100%)`
+  popup.src = ''
+  popup.alt = ''
+})
